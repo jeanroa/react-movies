ba8623e1a6da8561d8409873b8031f13c1f6b411
jeanroa <roajeanmarco@gmail.com>
2022-03-14 00:52:10 -0300

    se modifican archivos, probablemente tenga que empezar el proyecto desde 0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Moviesgrid } from './MoviesGrid';
+
+export const App = () => {
+  return <div>
+      <header>
+        <h1>Movies</h1>
+      </header>
+      <main>
+        <Moviesgrid/>
+      </main>
+    </div>
+}
+
---
+++ src/MoviesGrid.jsx
@@ -0,0 +1,9 @@
+export const Moviesgrid = ( ) => {
+  return (
+  <ul>
+    <li>Peli1</li>
+    <li>Peli2</li>
+    <li>Peli3</li>
+  </ul>
+  )
+}
